c0589a15d37ba405db2c8485b0ac93955e355214
Chris T Stuart <christstuart@gmail.com>
2014-11-08 17:36:00 -0500

    sdi-lab6/

--- sdi-lab6/StuartLajaraChris_Lab6.js
+++ sdi-lab6/StuartLajaraChris_Lab6.js
@@ -1,3 +1,7 @@
 var myTeams = function() {
 	var theTeams = [" Real Madrid"," FC Barcelona", " Atletico Madrid", " Valencia CF"];
 	var theTeamsPlace = [" Madrid.", " Barcelona.", " Madrid.", " Valencia."];
+for (var i = 0; i < theTeams.length; i++) {
+	console.log(theTeams[i] + " plays in" + theTeamsPlace[i]);
+	
+}
