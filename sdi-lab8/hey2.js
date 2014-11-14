fa6818376037dc7f4d7572ce1500d30149d7f4bc
Chris T Stuart <christstuart@gmail.com>
2014-11-13 21:27:23 -0500

    hey.js

--- sdi-lab8/hey2.js
+++ sdi-lab8/hey2.js
@@ -10,5 +10,16 @@ var myConfirmation = function() {
 var symbol = emailConfirmation.indexOf("@");
 var symbol1 = emailConfirmation.indexOf(".");
 
+if (emailConfirmation.length > 0 && emailConfirmation.indexOf("@@") > 0 || emailConfirmation == "") {
+	 return false;	 
+} else {
+	return true;
+}
+
+};
+	
+myAnswer = myConfirmation();
+
+console.log("It is " + myAnswer + " that your email is working.");
 
 
