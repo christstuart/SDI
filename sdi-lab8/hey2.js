41a5e0e721f23eb897e1b9c65cdd27af36983235
Chris T Stuart <christstuart@gmail.com>
2014-11-13 21:03:14 -0500

    hey2.js

---
+++ sdi-lab8/hey2.js
@@ -0,0 +1,14 @@
+// Chris T Stuart
+var emailConfirmation;
+var myAnswer;
+
+emailConfirmation = prompt("Enter email");
+
+
+var myConfirmation = function() {
+
+var symbol = emailConfirmation.indexOf("@");
+var symbol1 = emailConfirmation.indexOf(".");
+
+
+
