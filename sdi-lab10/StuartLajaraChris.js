7c491310945cb66f9e7aa24cd522f4c593ebb818
Chris T Stuart <christstuart@gmail.com>
2014-11-18 19:55:24 -0500

    sdi-lab10

--- sdi-lab10/StuartLajaraChris.js
+++ sdi-lab10/StuartLajaraChris.js
@@ -20,3 +20,32 @@ var myNumbers = function(num, num1) {
 done = myNumbers(numbers, numbers1);
 console.log(done + " is the sum of all the factors of 3 and 5");
 
+
+// problem 2
+
+
+var myLottery = function() {
+	
+	var myNumbersInside = [];
+	
+	
+	for ( var i = 0; i < 5; i++) {
+	    
+	 var myMath = Math.floor((Math.random() * (59 + 1) ) + 1 ) 
+		myNumbersInside.push(myMath);
+			
+	}
+	
+	for ( var i = 0; i < 1; i++) {
+	    
+	 var myPower = Math.floor(Math.random() * (35 + 1) ) 
+		myNumbersInside.push(myPower);
+			
+	}
+	
+	return myNumbersInside
+};
+
+result = myLottery();
+console.log(result[0] + "," + result[1] + "," + result[2] + "," + result[3] + "," + result[4] + " is your lottery number. " + result[5] + " is your Powerball.")
+
